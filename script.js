645ebe426f791c139a799357f07722e95b398615
Mrk3024 <940925oo@gmail.com>
2025-06-10 02:22:24 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleDescription(id) {
-  const desc = document.getElementById(id);
-  desc.classList.toggle("show");
-}
-
-const cursor = document.getElementById("turtle-cursor");
-document.addEventListener("mousemove", (e) => {
-  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
